f0a2203f1ae13b16fcf5885f70459d885d46e9a9
arunsachan <66840949+arunsachan@users.noreply.github.com>
2023-03-02 12:09:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your code here
+let iElement= document.getElementById('evaluatedText');
+let head= document.getElementById('wordCount');
+
+
+function handleInput(e){
+ let sentence = e.target.value;
+//   match(/\S+/ig) => this is one of the regex(regular expression) that we use if we want to know the number of words in a sentence. 
+
+ let match= sentence.match(/\S+/ig).length;
+ head.innerText=match;
+}
+
+iElement.addEventListener('input' , handleInput );
